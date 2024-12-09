8ae310cb4f9bd7eba5a5426b85524bb03df6cd3c
AobaIwaki123 <aiwaki@gmail.com>
2024-12-09 22:44:20 +0900

    fix sql

--- sql/4-custom-table.sql
+++ sql/4-custom-table.sql
@@ -1,3 +1,14 @@
+-- chairsテーブルからデータを挿入
 INSERT INTO chair_available (chair_id)
-SELECT chair_id
+SELECT id
 FROM chairs;
+
+-- 外部キー制約を追加（オプション）
+-- ALTER TABLE chair_available
+-- ADD CONSTRAINT fk_chair_id
+-- FOREIGN KEY (chair_id)
+-- REFERENCES chairs (id)
+-- ON DELETE CASCADE;
+
+-- インデックスの追加（オプション）
+-- CREATE INDEX idx_chair_id ON chair_available (chair_id);
